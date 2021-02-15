f20ffaa5e372050e37c8307b119f529b2c4dbcec
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2021-02-15 20:07:25 +0500

    questions generated

---
+++ find_wrong_answer.py
@@ -0,0 +1,13 @@
+from utilites import load, compare_phrase
+from pymorphy2 import MorphAnalyzer
+
+ma = MorphAnalyzer()
+
+terms = [ t.lower() for t in load('intuit_terms.json') ]
+
+def find_wrong_answer(answer = "операционной системы"):
+    return [ x[1] for x in sorted(( (compare_phrase(answer, x), x) for x in terms ), reverse=1)[3:6] ]
+    
+
+if __name__ == "__main__":
+    print(find_wrong_answer())
