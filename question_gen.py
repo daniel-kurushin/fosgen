a4eaec76298f61ba517865cc4fd35af832dba7dd
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2021-02-16 19:03:56 +0500

    exception on empty terms

--- question_gen.py
+++ question_gen.py
@@ -27,19 +27,22 @@ for weight, competence, sent_id in candidates:
     if weight > 0.3:
         sentence = sentences[sent_id]["sentence"]
         sent_terms = sorted([ (x,len(x)) for x in te(sentence, strings=1) ], key=lambda x:x[1], reverse=1)
-        q_term = sent_terms[0]
-        q_term_words = q_term[0].split()
-        sent_words = sentence.split()
-        stored = sentence
-        answer = []
-        for a, b in product(sent_words, q_term_words):
-            if compare(a, b) >= 0.5:
-                sentence = sentence.replace(a, "_", 1)
-                answer += [a]
-        if stored != sentence and not re.findall("_.{6,}_", sentence):
-            used |= set([sent_id])
-            answer = " ".join(answer)
-            wrong_answers = find_wrong_answer(answer)
-            questions.update({sent_id:[sentence, competence, answer, wrong_answers]})
+        try:
+            q_term = sent_terms[0]
+            q_term_words = q_term[0].split()
+            sent_words = sentence.split()
+            stored = sentence
+            answer = []
+            for a, b in product(sent_words, q_term_words):
+                if compare(a, b) >= 0.5:
+                    sentence = sentence.replace(a, "_", 1)
+                    answer += [a]
+            if stored != sentence and not re.findall("_.{6,}_", sentence):
+                used |= set([sent_id])
+                answer = " ".join(answer)
+                wrong_answers = find_wrong_answer(answer)
+                questions.update({sent_id:[sentence, competence, answer, wrong_answers]})
+        except IndexError:
+            pass
                 
 dump(questions, 'questions.json')
